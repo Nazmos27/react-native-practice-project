b24cd34f4db642a525938e6cce788199dcd4ec15
Nazmos27 <nazmos789@gmail.com>
2023-08-13 17:03:59 +0600

    sectionList Practice

--- App.js
+++ App.js
@@ -1,18 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LittleLemmonHeader from './components/LittleLemmonHeader';
 import LittleLemmonFooter from './components/LittleLemmonFooter';
-import LittleLemmonWlcm from './components/LittleLemmonWlcm';
-import MenuItems from './components/MenuItems';
-import MenuWithFlatList from './components/MenuWithFlatList';
-import FlatlistPractice from './components/FlatlistPractice';
+import SectionListPractice from './components/SectionListPractice';
 
 export default function App() {
   return (
-    // <View style={styles.container}>
-    //   <Text style={{fontSize:20}}>Hi,Hello World</Text>
-    //   <StatusBar style="auto" />
-    // </View>
+    
     <View style={{flex:1,backgroundColor: '#495E57'}}>
       <LittleLemmonHeader></LittleLemmonHeader>
       {/* <LittleLemmonWlcm></LittleLemmonWlcm>
@@ -21,7 +14,8 @@ export default function App() {
       </View> */}
       {/* <MenuItems></MenuItems> */}
       {/* <MenuWithFlatList></MenuWithFlatList> */}
-      <FlatlistPractice></FlatlistPractice>
+      {/* <FlatlistPractice></FlatlistPractice> */}
+      <SectionListPractice></SectionListPractice>
     </View>
   );
 }
